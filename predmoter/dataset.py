b15cadf4100c101ffd50f2a36f559852374adda1
fekin <fekin101@hhu.de>
2022-06-27 16:17:07 +0200

    modified get_item function and added correct decoding

--- predmoter/dataset.py
+++ predmoter/dataset.py
@@ -14,20 +14,25 @@ class PromoterDataset(Dataset):
     def __init__(self, h5_file, mode):
         self.h5_file = h5_file
         self.mode = mode
-        self.dataset = self.create_dataset(self.h5_file, self.mode)
+        self.dataset = self.create_dataset(self.h5_file)
 
     def __getitem__(self, idx):
-        return self.dataset[idx]  # get one chunk of the array
+        if self.mode == "test":
+            return self.decode_one(self.dataset[idx], np.int8, (self.sequence_length, self.nucleotides))
+        else:
+            return (self.decode_one(self.dataset[idx][0], np.int8, (self.sequence_length, self.nucleotides)),
+                    self.decode_one(self.dataset[idx][1], np.float32, (self.sequence_length,)))
 
-    def __len__(self):  # INCORRECT
+    def __len__(self):
         return len(self.dataset)
 
-    def create_dataset(self, file, mode):
+    def create_dataset(self, file):
         h5df = h5py.File(file, mode="r")
         X = np.array(h5df["data/X"], dtype=np.int8)
         # assert X.shape[1] == self.sequence_length, "H5 file has the wrong formatting."
-        if mode == "test":
-            return [self.compressor.encode(x) for x in X]  # do encode/decode for prediction?
+        if self.mode == "test":
+            array = [self.compressor.encode(x) for x in X]
+            return np.array(array)
 
         Y = np.array(h5df["evaluation/atacseq_coverage"], dtype=np.float32)
         assert np.shape(X)[:2] == np.shape(Y)[:2], "Size mismatch between input and labels."
@@ -36,12 +41,19 @@ class PromoterDataset(Dataset):
         mask = [len(y[y < 0]) == 0 for y in Y]  # exclude padded ends
         X = X[mask]
         Y = Y[mask]
-        return tuple((self.compressor.encode(x), self.compressor.encode(y)) for x, y in zip(X, Y))
+        arrays = tuple((self.compressor.encode(x), self.compressor.encode(y)) for x, y in zip(X, Y))
+        return np.array(arrays)  # should prevent num_workers from exceeding memory limit
+
+    def decode_one(self, array, np_dtype, shape):
+        array = np.frombuffer(self.compressor.decode(array), dtype=np_dtype)
+        array = np.reshape(array, shape)
+        array = np.array(array)
+        return torch.from_numpy(array).float()
 
 
 # has to be a class??
 class PromoterSequence:
-    def __init__(self, type_, input_dir, shuffle, batch_size):
+    def __init__(self, type_, input_dir, shuffle, batch_size, num_workers):
         self.type_ = type_
         assert self.type_ in ["train", "val", "test"], "Valid types are train, val or test."
         self.input_dir = "/".join([input_dir.rstrip("/"), self.type_])
@@ -50,23 +62,19 @@ class PromoterSequence:
         if type_ == "test":
             assert len(self.h5_files) == 1, "Predictions should only be applied to individual files."
 
-        self.datasets = [PromoterDataset(file, mode=self.type_) for file in self.h5_files]
+        self.dataset = ConcatDataset([PromoterDataset(file, mode=self.type_) for file in self.h5_files])
         self.shuffle = shuffle
         self.batch_size = batch_size
-        self.dataloader = self.create_dataloader(self.datasets, self.shuffle, self.batch_size)
+        self.num_workers = num_workers
+        self.dataloader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle,
+                                     pin_memory=True, num_workers=self.num_workers)
 
     def __getitem__(self, idx):
-        return self.datasets[idx]
+        return self.dataset[idx]
 
     def __len__(self):
-        return len(self.datasets)
-
-    @staticmethod
-    def create_dataloader(datasets, shuffle, batch_size):
-        dataset = ConcatDataset(datasets)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=True)
-        return dataloader
+        return len(self.dataset)
 
     # stuff to keep in mind:
-    # 1. compressor problem, right now: compressor = PromoterDataset.compressor in decode_one (also for seqlen & nucl.)
+    # 1. un-dynamic: PromoterDataset.sequence_length & nucleotides)
     # 2. trainer: train_dataloaders="variable".dataloader
