9a7d4f761ce35922f2e38e2daae78b3fba3d3a18
fekin <fekin101@hhu.de>
2022-09-08 13:15:53 +0200

    revoked X changes, Ns will be treated like padding

--- predmoter/dataset.py
+++ predmoter/dataset.py
@@ -39,22 +39,18 @@ class PredmoterSequence(Dataset):
             avg = h5df["evaluation/average_atacseq_coverage"][0]
             n, mem_size, chunks = 1000, 0, 0  # 1000
             for i in range(0, len(h5df["data/X"]), n):  # for data saving; len(h5df["data/X"])
-                X = np.array(h5df["data/X"][i:i + n], dtype=np.float32)
+                X = np.array(h5df["data/X"][i:i + n], dtype=np.int8)
                 if self.type_ != "test":
-                    mask = [np.sum(chunk) == X.shape[1] for chunk in X]
-                    # exclude padded ends; sum of all chunks should be equal to seq_len
                     Y = np.array(h5df["evaluation/atacseq_coverage"][i:i + n], dtype=np.float32)
-                    X, Y = X[mask], Y[mask]
-                    if len(X) == 0:
-                        continue  # skip if all 1000 chunks have padding
                     Y = np.mean(Y, axis=2)
+                    mask = np.sum(X, axis=2)  # zero for padding or Ns, dtype: int8 -> N = 0
+                    Y = Y * mask
                     Y = (Y / avg) * 4
                     Y = np.around(Y, 4)
                     assert np.shape(X)[:2] == np.shape(Y)[:2], "Size mismatch between input and labels."
                     Y = self.encode_one(Y)
                     mem_size += sys.getsizeof(Y)
                     Y_final.append(Y)
-                X = self.encode_one(X)
                 mem_size += sys.getsizeof(X)
                 X_final.append(X)
                 chunks += len(X)
@@ -78,17 +74,17 @@ def decode_one(array, dtype, shape):
     return torch.from_numpy(array).float()
 
 
-def collate_fn(batch_list, seq_len, bases):
+def collate_fn(batch_list, seq_len):
     # batch_list: list of dataset tuples with length=batch_size
     if len(batch_list[0]) == 2:  # train/val data
         X, Y = zip(*batch_list)
-        X = torch.stack([decode_one(x, np.float32, (seq_len, bases)) for x in X])
+        X = torch.stack([torch.from_numpy(x).float() for x in X])
         Y = torch.stack([decode_one(y, np.float32, (seq_len,)) for y in Y])
         return X, Y
-    return torch.stack([decode_one(x, np.float32, (seq_len, bases)) for x in batch_list])  # test data/just X
+    return torch.stack([torch.from_numpy(x).float() for x in batch_list])  # test data/just X
 
 
-def get_dataloader(input_dir, type_, batch_size, num_workers, seq_len, bases):
+def get_dataloader(input_dir, type_, batch_size, num_workers, seq_len):
     input_dir = os.path.join(input_dir, type_)
     h5_files = glob.glob(os.path.join(input_dir, "*.h5"))
     assert len(h5_files) >= 1, f"no input files with type {type_} were provided"
@@ -97,6 +93,6 @@ def get_dataloader(input_dir, type_, batch_size, num_workers, seq_len, bases):
     shuffle = True if type_ == "train" else False
     dataloader = DataLoader(PredmoterSequence(h5_files, type_), batch_size=batch_size,
                             shuffle=shuffle, pin_memory=True, num_workers=num_workers,
-                            collate_fn=lambda batch: collate_fn(batch, seq_len, bases))
+                            collate_fn=lambda batch: collate_fn(batch, seq_len))
     # lambda function makes it possible to add more arguments than just batch to collate_fn
     return dataloader
