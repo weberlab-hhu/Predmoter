f100304f551ad07810c6347b1c490039a281ea61
fekin <fekin101@hhu.de>
2022-10-25 12:10:38 +0200

    introduced model types: cnn, hybrid, bi-hybrid

--- predmoter/HybridModel.py
+++ predmoter/HybridModel.py
@@ -7,11 +7,12 @@ import pytorch_lightning as pl
 
 # add way more selves
 class LitHybridNet(pl.LightningModule):
-    def __init__(self, cnn_layers, filter_size, kernel_size, step, up,
+    def __init__(self, model_type, cnn_layers, filter_size, kernel_size, step, up,
                  hidden_size, lstm_layers, learning_rate, seq_len, input_size):
         super(LitHybridNet, self).__init__()
         self.seq_len = seq_len
         self.input_size = input_size
+        self.model_type = model_type
         self.cnn_layers = cnn_layers
         self.filter_size = filter_size
         self.kernel_size = kernel_size
@@ -27,10 +28,14 @@ class LitHybridNet(pl.LightningModule):
         # for checkpoints
         self.save_hyperparameters()
 
+        assert model_type in ["cnn", "hybrid", "bi-hybrid"], \
+            f"valid model types are cnn, hybrid and bi-hybrid not {model_type}"
+
         assert self.cnn_layers > 0, "at least one convolutional layer is required"
 
         assert (self.seq_len % self.step ** self.cnn_layers) == 0, \
-            f"sequence length {self.seq_len} is not divisible by {self.step} to the power of {self.cnn_layers}"
+            f"sequence length {self.seq_len} is not divisible by a step of {self.step} " \
+            f"to the power of {self.cnn_layers} cnn layers"
 
         # CNN part:
         # --------------------
@@ -48,23 +53,32 @@ class LitHybridNet(pl.LightningModule):
             input_size = filter_size
             filter_size = filter_size * self.up
 
-        # LSTM part:
-        # --------------------
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size,
-                            num_layers=self.lstm_layers, batch_first=True)
-        # input: last dimension of tensor, output:(batch,new_seq_len,hidden_size)
+        if model_type != "cnn":
+            # LSTM part:
+            # --------------------
+            bidirectional = True if model_type == "bi-hybrid" else False
+            self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size,
+                                num_layers=self.lstm_layers, batch_first=True, bidirectional=bidirectional)
+            # input: last dimension of tensor, output:(batch,new_seq_len,hidden_size)
 
         # Transposed CNN part
         # --------------------
         self.up_layer_list = nn.ModuleList()  # up part of the U-net
+        out_pad = 1 if self.step % 2 == 0 else 0  # uneven step doesn't need output_padding
+
+        if model_type == "cnn":
+            hidden_size = input_size  # was set to the last filter_size in the cnn part
+
+        if model_type == "bi-hybrid":
+            hidden_size = hidden_size * 2  # bidirectional doubles hidden size
 
         for layer in range(cnn_layers, 0, -1):  # count layers backwards
             l_in = self.seq_len / self.step ** layer
             l_out = l_in * self.step
-            tpad = self.trans_padding(l_in, l_out, self.step, 1, self.kernel_size, 1)  # dilation & output_padding = 1
+            tpad = self.trans_padding(l_in, l_out, self.step, 1, self.kernel_size, out_pad)  # dilation=1
             filter_size = self.filter_list[layer-1]  # iterate backwards over filter_list
             self.up_layer_list.append(nn.ConvTranspose1d(hidden_size, filter_size, self.kernel_size, stride=self.step,
-                                                         padding=tpad, dilation=1, output_padding=1))
+                                                         padding=tpad, dilation=1, output_padding=out_pad))
             hidden_size = filter_size
 
         # Linear part:
@@ -79,17 +93,18 @@ class LitHybridNet(pl.LightningModule):
         for layer in self.cnn_layer_list:
             x = F.relu(layer(x))
 
-        x = x.transpose(1, 2)
+        if self.model_type != "cnn":
+            x = x.transpose(1, 2)
 
-        # LSTM part:
-        # --------------------
-        # size of hidden and cell state: (num_layers,batch_size,hidden_size)
-        x, _ = self.lstm(x)  # _=(hn, cn)
+            # LSTM part:
+            # --------------------
+            # size of hidden and cell state: (num_layers,batch_size,hidden_size)
+            x, _ = self.lstm(x)  # _=(hn, cn)
+
+            x = x.transpose(1, 2)
 
         # Transposed CNN part
         # --------------------
-        x = x.transpose(1, 2)
-
         for layer in self.up_layer_list:
             x = F.relu(layer(x))
 
@@ -118,6 +133,7 @@ class LitHybridNet(pl.LightningModule):
         pred = self(X)
         mask = torch.sum(X, dim=2)  # zero for any predictions based on padding or Ns
         pred = pred * mask
+        Y = Y * mask
         loss = F.poisson_nll_loss(pred, Y, log_input=True)
         acc = self.pear_coeff(pred, Y, is_log=True)
         return loss, acc
@@ -137,9 +153,8 @@ class LitHybridNet(pl.LightningModule):
 
     @staticmethod
     def trans_padding(l_in, l_out, stride, dilation, kernel_size, output_pad):
-        padding = math.ceil(((l_in - 1) * stride - l_out + dilation * (kernel_size - 1) + output_pad + 1) / 2)
-        # math.ceil to  avoid rounding half to even/bankers rounding
-        return padding
+        padding = ((l_in - 1) * stride - l_out + dilation * (kernel_size - 1) + output_pad + 1) / 2
+        return int(padding)
 
     @staticmethod
     def pear_coeff(prediction, target, is_log=True):
@@ -166,6 +181,9 @@ class LitHybridNet(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         group = parent_parser.add_argument_group("Model_arguments")
+        group.add_argument("--model-type", type=str, default="hybrid",
+                           help="The type of model to train. Valid types are cnn, hybrid (CNN + LSTM) and "
+                                "bi-hybrid (CNN + BiLSTM). (default: %(default)d)")
         group.add_argument("--cnn-layers", type=int, default=1, help="(default: %(default)d)")
         group.add_argument("--filter-size", type=int, default=64, help="(default: %(default)d)")
         group.add_argument("--kernel-size", type=int, default=9, help="(default: %(default)d)")
