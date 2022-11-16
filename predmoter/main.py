98298cb6012c49d11684d7893aee1beebd8e348e
fekin <fekin101@hhu.de>
2022-11-16 13:34:50 +0100

    able to handle multiple NGS datasets

--- predmoter/main.py
+++ predmoter/main.py
@@ -9,8 +9,8 @@ from utils import check_paths, init_logging, set_seed, get_meta, set_callbacks
 from HybridModel import LitHybridNet
 
 
-def main(model_arguments, input_directory, output_directory, mode, resume_training, model, seed,
-         checkpoint_path, quantity, patience, batch_size, test_batch_size, num_workers, prefix, device,
+def main(model_arguments, input_directory, output_directory, mode, resume_training, model, datasets, seed,
+         checkpoint_path, quantity, patience, batch_size, test_batch_size, prefix, device,
          num_devices, epochs, limit_predict_batches):
 
     # Argument checks and cleanup
@@ -27,6 +27,7 @@ def main(model_arguments, input_directory, output_directory, mode, resume_traini
     # --------------------------------
     init_logging(output_directory, prefix)
     logging.info(f"Predmoter is starting in {mode} mode.")
+    logging.info(f"Chosen dataset(s): {' '.join(datasets)}")
     if mode == "train":
         set_seed(seed)
     seq_len, bases = get_meta(input_directory, mode)
@@ -44,7 +45,7 @@ def main(model_arguments, input_directory, output_directory, mode, resume_traini
             f"Your chosen model has the input size {hybrid_model.input_size} and your dataset {bases}." \
             f"Please use the same input size."  # how often would this actually happen?
     else:
-        hybrid_model = LitHybridNet(**model_arguments, seq_len=seq_len, input_size=bases)
+        hybrid_model = LitHybridNet(**model_arguments, seq_len=seq_len, input_size=bases, output_size=len(datasets))
     logging.info(f"\n\nModel summary (model type: {hybrid_model.model_type}):"
                  f"\n{ModelSummary(model=hybrid_model, max_depth=-1)}\n")
 
@@ -59,9 +60,9 @@ def main(model_arguments, input_directory, output_directory, mode, resume_traini
     # --------------------------------
     if mode == "train":
         train_loader = get_dataloader(input_dir=input_directory, type_="train", batch_size=batch_size,
-                                      num_workers=num_workers)
+                                      seq_len=seq_len, datasets=datasets)
         val_loader = get_dataloader(input_dir=input_directory, type_="val", batch_size=batch_size,
-                                    num_workers=num_workers)
+                                    seq_len=seq_len, datasets=datasets)
         logging.info(f"Training started. Resuming training: {resume_training}.")  # to print callback at some point
         if resume_training:
             trainer.fit(model=hybrid_model, train_dataloaders=train_loader,
@@ -73,14 +74,14 @@ def main(model_arguments, input_directory, output_directory, mode, resume_traini
 
     elif mode == "validate":
         val_loader = get_dataloader(input_dir=input_directory, type_="val", batch_size=batch_size,
-                                    num_workers=num_workers)
+                                    seq_len=seq_len, datasets=datasets)
         logging.info("Validation started.")
         trainer.validate(model=hybrid_model, dataloaders=val_loader)
         logging.info("Validation ended.")
 
     elif mode == "predict":
         test_loader = get_dataloader(input_dir=input_directory, type_="test", batch_size=test_batch_size,
-                                     num_workers=num_workers)
+                                     seq_len=seq_len, datasets=datasets)
         logging.info("Predicting started.")
         predictions = trainer.predict(model=hybrid_model, dataloaders=test_loader)
         logging.info("Predicting ended.")
@@ -96,7 +97,7 @@ if __name__ == "__main__":
     parser = LitHybridNet.add_model_specific_args(parser)  # add model specific args
     model_args = parser.parse_known_args()
     parser.add_argument("-h", "--help", action="help", help="show this help message and exit")  # not included in args
-    parser.add_argument("--version", action="version", version="%(prog)s 0.8")  # not included in args
+    parser.add_argument("--version", action="version", version="%(prog)s 0.9")  # not included in args
     parser.add_argument("-i", "--input-directory", type=str, default=".",
                         help="containing one train and val directory for training "
                              "and/or a test directory for predicting (directories must contain h5 files)")
@@ -110,6 +111,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, default="last.ckpt",
                         help="model checkpoint file used for predictions or resuming training (if not in "
                              "checkpoint directory, provide full path")
+    parser.add_argument("--datasets", type=str, nargs="+", dest="datasets", default=["atacseq", "h3k4me3"],
+                        help="the dataset(s) the network will train on")
     parser.add_argument("--seed", type=int, default=None, help="if not provided: will be chosen randomly")
     parser.add_argument("--checkpoint-path", type=str, default=None,
                         help="only specify, if other path than output_directory/checkpoints is preferred")
@@ -121,12 +124,11 @@ if __name__ == "__main__":
                         help="allowed epochs without the quantity improving before stopping training")
     parser.add_argument("-b", "--batch-size", type=int, default=195, help="batch size for training and validation data")
     parser.add_argument("--test-batch-size", type=int, default=100, help="batch size for test data")
-    parser.add_argument("--num-workers", type=int, default=0, help="for multiprocessing during data loading")
     parser.add_argument("--prefix", type=str, default=None, help="prefix for metric, log and prediction files")
     group = parser.add_argument_group("Trainer_arguments")
     group.add_argument("--device", type=str, default="gpu", help="device to train on")
-    group.add_argument("--num-devices", type=int, default=1, help="(default: %(default)d)")
-    group.add_argument("-e", "--epochs", type=int, default=35, help="(default: %(default)d)")
+    group.add_argument("--num-devices", type=int, default=1, help="number of devices to train on (default recommended)")
+    group.add_argument("-e", "--epochs", type=int, default=35, help="number of training runs")
     group.add_argument("--limit-predict-batches", action="store", dest="limit_predict_batches",
                        help="limiting predict: float = fraction, int = num_batches "
                             "(if not specified, will default to 1.0)")
