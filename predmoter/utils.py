0d149961d93b9e6bc64ef7519be42827e8d794e1
fekin <fekin101@hhu.de>
2023-01-23 15:36:10 +0100

    predictions to h5 files; seed states for reproducibility

--- predmoter/utils.py
+++ predmoter/utils.py
@@ -4,12 +4,11 @@ import logging
 import glob
 import time
 import numpy as np
-# noinspection PyUnresolvedReferences
-import torch  # for item() in metric callback
+import torch
 import h5py
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, Callback
 from pytorch_lightning.utilities.seed import seed_everything
-#  Please use `lightning_lite.utilities.seed.seed_everything` instead.
+#  Please use `lightning_lite.utilities.seed.seed_everything` for Pytorch Lightning v2.0.0 and upwards instead.
 
 
 class MetricCallback(Callback):
@@ -24,16 +23,17 @@ class MetricCallback(Callback):
         metrics = trainer.callback_metrics  # does not add validation sanity check
         msg = " ".join([str(epoch)] + [str(m.item()) for m in list(metrics.values())])
         if epoch == 0:
-            msg = " ".join(list(metrics.keys())) + "\n" + msg  # self.metric_names
+            msg = " ".join(["epoch"] + list(metrics.keys())) + "\n" + msg  # self.metric_names
         self.save_metrics(msg)
 
     def on_test_end(self, trainer, pl_module):
         metrics = trainer.callback_metrics
-        # just one test dataloader available
+        # test_dataloaders[0] --> just one test dataloader is available
+        # split() removes the filepath
         input_filename = trainer.test_dataloaders[0].dataset.h5_files[0].split("/")[-1]
         msg = f"{input_filename} " + " ".join([str(m.item()) for m in list(metrics.values())])
         if not os.path.exists(self.file):  # if the file is already there the header is not needed
-            msg = "species " + " ".join(list(metrics.keys())) + "\n" + msg
+            msg = " ".join(["species"] + list(metrics.keys())) + "\n" + msg
         self.save_metrics(msg)
 
     def save_metrics(self, msg):
@@ -65,14 +65,117 @@ class Timeit(Callback):
         log_table([str(self.last_epoch), f"{sum(self.durations)/60**2:.2f} h"], spacing=16, table_end=True)
 
 
-def set_callbacks(output_dir, mode, prefix, checkpoint_path, ckpt_quantity, save_top_k, stop_quantity, patience):
+# noinspection PyTypedDict
+# for this class only
+class SeedCallback(Callback):
+    def __init__(self, seed):
+        self.state = {"seed": seed, "random_seed_state": None, "numpy_seed_state": None,
+                      "torch_seed_state": None, "torch.cuda_seed_state": None}
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        self.state["random_seed_state"] = random.getstate()
+        self.state["numpy_seed_state"] = np.random.get_state()
+        self.state["torch_seed_state"] = torch.get_rng_state()
+        self.state["torch.cuda_seed_state"] = torch.cuda.get_rng_state()
+
+    def load_state_dict(self, state_dict):
+        self.state.update(state_dict)
+
+    def state_dict(self):
+        return self.state.copy()
+
+
+class PredictCallback(Callback):
+    def __init__(self, prefix, output_dir, add_additional, model, datasets):
+        self.prefix = prefix
+        self.output_dir = output_dir
+        self.add_additional = add_additional
+        self.model = model
+        self.datasets = datasets
+        self.input_file = None
+        self.output_file = None
+        self.length = None
+        self.pred_key = None
+        self.start = 0
+        self.stop = None
+        self.batch_size = None
+
+    def on_predict_start(self, trainer, pl_module):
+        # get input file meta data
+        # -------------------------
+        # predict_dataloaders[0] --> just one predict dataloader is available
+        self.input_file = trainer.predict_dataloaders[0].dataset.h5_files[0]
+        # split() removes the filepath and file extension
+        input_name = self.input_file.split("/")[-1].split(".")[0]
+        in_h5df = h5py.File(self.input_file, "r")
+        self.length = in_h5df["data/X"].shape[0]
+        chunk_len = in_h5df["data/X"].shape[1]
+
+        # define the h5 output file
+        self.output_file = os.path.join(self.output_dir, f"{self.prefix}{input_name}_predictions.h5")
+        h5df = h5py.File(self.output_file, "a")
+
+        if self.add_additional is None:
+            # data group setup
+            # --------------------
+            h5df.create_group("data")
+            h5df.create_dataset("data/species", shape=(self.length,), maxshape=(None,), dtype="S50",
+                                fillvalue=in_h5df["data/species"][0])
+            h5df.create_dataset("data/seqids", shape=(self.length,), maxshape=(None,), dtype="S50",
+                                data=np.array(in_h5df["data/seqids"]))
+            h5df.create_dataset("data/start_ends", shape=(self.length, 2), maxshape=(None, 2), dtype=int,
+                                data=np.array(in_h5df["data/start_ends"]))
+
+        # prediction groups
+        # ---------------------
+        if self.add_additional is not None and "alternative" not in h5df.keys():
+            h5df.create_group("alternative")
+
+        self.pred_key = "prediction" if self.add_additional is None else \
+            f"alternative/{self.add_additional}_prediction"
+        h5df.create_group(self.pred_key)
+        h5df.create_dataset(self.pred_key + "/predictions", shape=(self.length, chunk_len, len(self.datasets)),
+                            maxshape=(None, chunk_len, None), chunks=(1, chunk_len, 1), dtype=int,
+                            compression="gzip", compression_opts=9, fillvalue=-1)
+
+        h5df.create_dataset(self.pred_key + "/prediction_meta/model_name", shape=(1,), maxshape=(None,),
+                            dtype="S256", fillvalue=self.model.encode("ASCII"))
+        h5df.create_dataset(self.pred_key + "/prediction_meta/datasets", shape=(len(self.datasets),),
+                            maxshape=(None,), dtype="S25", data=np.array(self.datasets, dtype="S50"))
+
+        in_h5df.close()
+        h5df.close()
+
+    def on_predict_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        if self.stop is None:
+            self.batch_size = outputs.size(0)
+            self.stop = self.batch_size
+
+        if self.stop > self.length:  # length = length of input data
+            self.stop = self.length
+
+        h5df = h5py.File(self.output_file, "a")
+        # round, so the outputs are integers
+        h5df[f"{self.pred_key}/predictions"][self.start:self.stop] = np.array(outputs.cpu()).round(decimals=0)
+
+        self.start += self.batch_size
+        self.stop += self.batch_size
+
+        h5df.flush()
+        h5df.close()
+
+
+def set_callbacks(output_dir, mode, prefix, seed, checkpoint_path, ckpt_quantity, save_top_k,
+                  stop_quantity, patience, add_additional, model, datasets):
     if mode == "predict":
-        return None
+        predict_callback = PredictCallback(prefix, output_dir, add_additional, model, datasets)
+        return [predict_callback]
 
     metrics_callback = MetricCallback(output_dir, mode, prefix)
     if mode == "validate":
         return [metrics_callback]
 
+    seed_callback = SeedCallback(seed)
     for quantity in [ckpt_quantity, stop_quantity]:
         assert quantity in ["avg_train_loss", "avg_train_accuracy", "avg_val_loss", "avg_val_accuracy"],\
             f"can not monitor invalid quantity: {quantity}"
@@ -87,7 +190,7 @@ def set_callbacks(output_dir, mode, prefix, checkpoint_path, ckpt_quantity, save
                                mode=stop_method, strict=True, check_finite=True,
                                check_on_train_epoch_end=True)
     time_callback = Timeit()
-    callbacks = [checkpoint_callback, metrics_callback, early_stop, time_callback]
+    callbacks = [checkpoint_callback, metrics_callback, seed_callback, early_stop, time_callback]
     return callbacks
 
 
@@ -98,6 +201,20 @@ def set_seed(seed):
     else:
         logging.info(f"The seed provided by the user is: {seed}.")
     seed_everything(seed=seed, workers=True)  # seed for reproducibility
+
+
+def set_seed_state(model):
+    # path to the model checkpoint
+    model_dict = torch.load(model)
+    seed_dict = model_dict["callbacks"]["SeedCallback"]
+
+    # does basically the same as seed_everything(), but reloads states from checkpoint
+    os.environ["PL_GLOBAL_SEED"] = str(seed_dict["seed"])
+    random.setstate(seed_dict["random_seed_state"])
+    np.random.set_state(seed_dict["numpy_seed_state"])
+    torch.set_rng_state(seed_dict["torch_seed_state"])
+    torch.cuda.set_rng_state(seed_dict["torch.cuda_seed_state"])
+    logging.info(f"The seed provided by the model is: {seed_dict['seed']}.")
 
 
 class CustomFormatter(logging.Formatter):
@@ -164,3 +281,10 @@ def get_available_datasets(h5_file, model_datasets):
     h5df = h5py.File(h5_file, mode="r")
     avail_datasets = [dataset for dataset in model_datasets if f"{dataset}_coverage" in h5df["evaluation"].keys()]
     return avail_datasets
+
+
+def check_alternative_prediction(h5_file, pred_name):
+    h5df = h5py.File(h5_file, mode="r")
+    if "alternative" in h5df.keys():
+        assert f"{pred_name}_prediction" not in h5df["alternative"].keys(), \
+            f"alternative/{pred_name}_prediction is already in {h5_file}"
