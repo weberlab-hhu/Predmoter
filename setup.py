2bf5cf1cff0224354979318f61b0241a7a1d5b34
felicitas215 <12wahrheit@gmail.com>
2023-05-12 17:41:47 +0200

    expand setup

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup
+from predmoter.core.constants import PREDMOTER_VERSION  # works without install???
 
-setup()
+# exec(open("predmoter/core/constants.py").read())  # works
+
+setup(
+   name="predmoter",
+   version=PREDMOTER_VERSION,
+   author="Felicitas Kindel",
+   url="https://github.com/weberlab-hhu/Predmoter",
+   description="Deep Learning model predicting ATAC- and ChIP-seq data",
+   packages=["predmoter", "predmoter.core", "predmoter.prediction", "predmoter.utilities"],
+   scripts=["Predmoter.py"]
+)
+# later: package_data, install_requires (packages/git), dependency_links
